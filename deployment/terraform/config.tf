a009398271c93031285516812c84700bb46acc9b
Taylor Nation <tnation14@gmail.com>
2018-07-13 14:50:37 -0400

    Pin terraform version to 0.10.8

--- deployment/terraform/config.tf
+++ deployment/terraform/config.tf
@@ -1,5 +1,14 @@
 provider "aws" {
-  region = "us-east-1"
+  region  = "us-east-1"
+  version = "~> 1.27.0"
+}
+
+provider "template" {
+  version = "~> 1.0.0"
+}
+
+provider "terraform" {
+  version = "~> 1.0.0"
 }
 
 terraform {
